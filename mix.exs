43e33238ef1960cf4b77f9ba9d9e366712b02c3d
Tomoki Odaka <tomoki.odaka@aktsk.jp>
2021-08-27 14:24:52 +0900

    Include :eex in extra_applications

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule ProtoValidator.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:eex, :logger]
     ]
   end
 
